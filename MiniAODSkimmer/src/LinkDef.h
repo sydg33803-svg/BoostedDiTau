ec0cb56cbd3028423ac1c26501486ae1d1d97fad
Sydney Gunn <sydg33803@gmail.com>
2026-06-17 21:42:44 +0200

    add LinkDef

---
+++ MiniAODSkimmer/src/LinkDef.h
@@ -0,0 +1,18 @@
+1;95;0c#include "BoostedDiTau/MiniAODSkimmer/interface/JetInfoDS.h"
+#include "BoostedDiTau/MiniAODSkimmer/interface/MuonInfoDS.h"
+#include "BoostedDiTau/MiniAODSkimmer/interface/ElectronInfoDS.h"
+#include "BoostedDiTau/MiniAODSkimmer/interface/TauInfoDS.h"
+#include "BoostedDiTau/MiniAODSkimmer/interface/PhotonInfoDS.h"
+
+#ifdef __CINT__
+#pragma link C++ class JetInfo+;
+#pragma link C++ class JetInfoDS+;
+#pragma link C++ class MuonInfo+;
+#pragma link C++ class MuonInfoDS+;
+#pragma link C++ class ElectronInfo+;
+#pragma link C++ class ElectronInfoDS+;
+#pragma link C++ class TauInfo+;
+#pragma link C++ class TauInfoDS+;
+#pragma link C++ class PhotonInfo+;
+#pragma link C++ class PhotonInfoDS+;
+#endif
